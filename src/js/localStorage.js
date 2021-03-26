32db8d1747ae4e3a595036635c8d58cb1dc310fd
Mariia <smimi_00@mail.ru>
2021-03-26 21:20:46 +0300

    refactored js and added update-info

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?"; // ?q={city}&appid={api-key}
-const API_KEY = "b837cfa721addad4d37a808828acf5df";
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather'; // ?q={city}&appid={api-key}
+const API_KEY_PARAM_NAME = 'appid';
+const CITY_PARAM_NAME = 'q';
+const API_KEY = 'b837cfa721addad4d37a808828acf5df';
+const WEATHER_URL = new URL(API_URL);
+WEATHER_URL.searchParams.set(API_KEY_PARAM_NAME, API_KEY);
+
 const HERE_CITY = "here-city";
 const HERE_CITY_COORDS = "here-city-coords";
 
@@ -20,7 +25,12 @@ if (localStorage.getItem(HERE_CITY) == null && localStorage.getItem(HERE_CITY_CO
     });
 }
 
+
+
 // TODO waiting loading info
+let update_info = document.getElementById("here-city-update-info").cloneNode(true);
+update_info.
+update_info.classList.add();
 
 // TODO getting weather for here-city and favorites
 
@@ -29,7 +39,7 @@ if (localStorage.getItem(HERE_CITY) == null && localStorage.getItem(HERE_CITY_CO
 
 // getting info example
 let xhr = new XMLHttpRequest();
-xhr.open('GET', API_URL + "q=London&appid=" + API_KEY);
+xhr.open('GET', API_URL + '?q=London&appid=' + API_KEY);
 xhr.send();
 xhr.responseType = 'json';
 let x;
